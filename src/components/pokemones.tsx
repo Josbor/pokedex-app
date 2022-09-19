3712b029eddc806ffc6f6d1931d41980949c0df0
Josbor <josbor.dev@gmail.com>
2022-09-19 15:09:42 -0400

    se uso el context para centralizar la informacion

--- src/components/pokemones.tsx
+++ src/components/pokemones.tsx
@@ -1,28 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { getPokemonsContext, pokemonsContext, useGetPokemonsContext, usePokemonsContext } from '../contexts/PokemonProvider';
 import { getApi } from '../helpers/getApi';
+import { pokemones as pokemonObjecto } from '../models/pokemons';
 import Pokemon from './pokemon';
-
+//import {pokemones} from '../models/pokemons'
 const pokemones = () => {
-    const url:string='https://pokeapi.co/api/v2/pokemon?limit=20&offset=20'
-
-    const [pokemones,setPokemones]=useState<any[]>([]);
-  
-     useEffect(
-        
-       () =>{
-       
-        getApi(url,setPokemones);
-  
-       
-      },[]
-  );
-  
-  
+    //const url:string='https://pokeapi.co/api/v2/pokemon?limit=20&offset=20'
+    const getAllPokemons=useContext(getPokemonsContext);
+    //getAllPokemons()
+    const pokemonContext=useContext<pokemonObjecto[]>(pokemonsContext)
+    const [PokemonsObject,setpokemonObject]=useState<any[]|null>()
+    
     return (
     <div>
         <div className='container_pokemons'>
-              { 
-              pokemones.map(pokemones=>{
+                      { 
+              pokemonContext?.map(pokemones=>{
                   
                   return(
                     <Pokemon
