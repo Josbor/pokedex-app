64c7fd5cd3fe623a56cf9abc976df2397e726193
Josbor <josbor.dev@gmail.com>
2022-09-20 22:59:50 -0400

    update

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,19 +1,34 @@
 import React, { useContext } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import { getPokemonInfoContext, pokemonContext } from '../contexts/PokemonProvider'
+import useEffect from 'react';
 
 const PokemonInfo = () => {
   const location=useLocation() 
-  console.log("🚀 ~ file: PokemonInfo.tsx ~ line 7 ~ PokemonInfo ~ pokemones", location.state.pokemones)
+  const pokemones=location.state
+  
+  // useEffect(()=>{
+  //     // llamado de la api
+
+  // }
+    
+    
+  //   ,[])
 
   
   const getpokeInfo=useContext(getPokemonInfoContext)
   const pokeInfo=useContext(pokemonContext)
   getpokeInfo(name);
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <div className='container_pokemon' >
+                <img src={pokemones.image} alt="" />
+                <div className='container_info'>
+                    <h2>{pokemones.name}</h2>
+                    
+                </div>
+            
+
+        </div>
   )
 }
 
